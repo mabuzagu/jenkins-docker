edb98d4c5987b053e88af1da21568e548d31bc11
Gugu Mabuza <mabuzagu@gmail.com>
2018-04-16 21:03:35 -0400

    Home folder default

--- launch.sh
+++ launch.sh
@@ -4,8 +4,8 @@ docker run -d \
   --privileged \
   -e DOCKER_DAEMON_ARGS="-H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock" \
   --restart=always \
-  -v /home/schlechtweg/jenkins:/var/lib/jenkins \
-  -v /home/schlechtweg/sonarqube/conf:/var/lib/sonarqube-6.4/conf \
-  -v /home/schlechtweg/sonarqube/data:/var/lib/sonarqube-6.4/data \
+  -v ~/schlechtweg/jenkins:/var/lib/jenkins \
+  -v ~/schlechtweg/sonarqube/conf:/var/lib/sonarqube-6.4/conf \
+  -v ~/schlechtweg/sonarqube/data:/var/lib/sonarqube-6.4/data \
   --name insizaJenkins \
   schlechtweg/jenkins-nodejs
